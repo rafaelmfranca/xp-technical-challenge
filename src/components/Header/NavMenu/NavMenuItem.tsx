e9d2c98a5ad2b0598fccf983f6bb04b88ba6603d
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-16 15:12:32 -0300

    feat: add nav menu item component

---
+++ src/components/Header/NavMenu/NavMenuItem.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+
+type NavMenuItemProps = {
+  href: string;
+  title: string;
+  icon: ReactNode;
+};
+
+export default function NavMenuItem({ href, title, icon }: NavMenuItemProps) {
+  const router = useRouter();
+
+  return (
+    <li>
+      <Link href={href}>
+        <a
+          className={
+            router.asPath === href ? 'bg-primary text-primary-content' : ''
+          }
+        >
+          {icon}
+          <span className="font-medium">{title}</span>
+        </a>
+      </Link>
+    </li>
+  );
+}

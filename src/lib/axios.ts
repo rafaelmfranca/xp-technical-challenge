4516fd16e5348122ae8254c62310d8bd3b5ba671
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-16 00:25:59 -0300

    feat: add prisma and axios config

---
+++ src/lib/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.API_URL
+    : 'http://localhost:3000';
+
+export const api = axios.create({ baseURL });
+
+export const fetcher = async (url: string) => {
+  const { data } = await api.get(url);
+  return data;
+};

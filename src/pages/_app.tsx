f0ffa3cc4d058e99dd0f24f1840c14bbaea40f59
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-16 15:05:31 -0300

    feat: add theme provider, add phospor-react icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import AuthProvider from '@/contexts/auth';
+import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider defaultTheme="system">
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }

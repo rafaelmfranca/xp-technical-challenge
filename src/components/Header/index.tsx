66b51b493b0217597c47c01ad206a83b867efc31
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-16 15:06:20 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import NavBar from './NavBar';
+
+export default function Header() {
+  return (
+    <header>
+      <NavBar />
+    </header>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 import ErrorToast from './ErrorToast';
 import Button from './Form/Button';
 import Input from './Form/Input';
+import Header from './Header';
 import Hero from './Hero';
 import LoginForm from './LoginForm';
 
-export { Input, Hero, LoginForm, Button, ErrorToast };
+export { Input, Hero, LoginForm, Button, ErrorToast, Header };

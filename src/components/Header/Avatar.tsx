ae2dfff21096ae9fde6061ed95eb266236b561e4
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-16 15:11:41 -0300

    feat(wip): add avatar component

---
+++ src/components/Header/Avatar.tsx
@@ -0,0 +1,9 @@
+export default function Avatar() {
+  return (
+    <div className="avatar online placeholder" role="img" aria-label="Avatar">
+      <div className="w-10 rounded-full bg-neutral text-neutral-content">
+        <span className="text-md">xp</span>
+      </div>
+    </div>
+  );
+}

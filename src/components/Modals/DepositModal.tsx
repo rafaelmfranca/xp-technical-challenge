f321fcf121cf5d6984c1f1c21ed7f3278f053fbc
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-18 13:55:18 -0300

    feat: add deposit modal component

---
+++ src/components/Modals/DepositModal.tsx
@@ -0,0 +1,77 @@
+import useAccount from '@/hooks/useAccount';
+import depositSchema from '@/schemas/deposit';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useSWRConfig } from 'swr';
+import Input from '../Form/Input';
+
+type DepositFormData = {
+  amount: number;
+};
+
+export default function DepositModal() {
+  const { mutate } = useSWRConfig();
+  const { handleAddDeposit } = useAccount();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm<DepositFormData>({
+    mode: 'onChange',
+    resolver: yupResolver(depositSchema),
+  });
+
+  const handleSubmitDeposit: SubmitHandler<DepositFormData> = async (data) => {
+    await handleAddDeposit({ ...data, type: 'DEPOSIT' });
+    mutate('/api/conta');
+    reset();
+
+    // i will fix that
+    const modal = document.getElementById('deposit-modal') as any;
+    modal.checked = false;
+  };
+
+  return (
+    <>
+      <input type="checkbox" id="deposit-modal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <label
+            htmlFor="deposit-modal"
+            className="absolute btn btn-sm btn-circle right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">Insira o valor do depósito</h3>
+          <form
+            onSubmit={handleSubmit((data) =>
+              handleSubmitDeposit(data as DepositFormData),
+            )}
+          >
+            <div className="py-4">
+              <Input
+                {...register('amount')}
+                name="amount"
+                type="number"
+                label="Valor"
+                step=".01"
+                error={errors.amount}
+              />
+            </div>
+            <div className="mt-0 modal-action">
+              <button
+                type="submit"
+                className={`btn btn-primary ${isSubmitting && 'loading'}`}
+                disabled={!isValid}
+              >
+                Confirmar
+                <label htmlFor="deposit-modal" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}

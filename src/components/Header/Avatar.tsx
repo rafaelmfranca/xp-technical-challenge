c69708c15303a5a33ff5a9853c13e78190fc96b5
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-18 15:24:42 -0300

    refactor: update avatar component

--- src/components/Header/Avatar.tsx
+++ src/components/Header/Avatar.tsx
@@ -1,8 +1,12 @@
+import useSession from '@/hooks/useSession';
+
 export default function Avatar() {
+  const { email } = useSession();
+
   return (
     <div className="avatar online placeholder" role="img" aria-label="Avatar">
       <div className="w-10 rounded-full bg-neutral text-neutral-content">
-        <span className="text-md">xp</span>
+        <span className="text-md">{email.substring(0, 2)}</span>
       </div>
     </div>
   );

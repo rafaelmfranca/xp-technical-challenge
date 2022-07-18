7a35a43df112c4dae35b946f18f17b43fea4e4a7
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-18 13:56:19 -0300

    feat: add withdrawal modal component

---
+++ src/components/Modals/WithdrawalModal.tsx
@@ -0,0 +1,79 @@
+import useAccount from '@/hooks/useAccount';
+import generateWithdrawalSchema from '@/schemas/withdrawal';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useSWRConfig } from 'swr';
+import Input from '../Form/Input';
+
+type WithdrawalFormData = {
+  amount: number;
+};
+
+export default function WithdrawalModal() {
+  const { mutate } = useSWRConfig();
+  const { handleAddWithdrawal, balance } = useAccount();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm<WithdrawalFormData>({
+    mode: 'onChange',
+    resolver: yupResolver(generateWithdrawalSchema(balance)),
+  });
+
+  const handleSubmitWithdrawal: SubmitHandler<WithdrawalFormData> = async (
+    data,
+  ) => {
+    await handleAddWithdrawal({ ...data, type: 'WITHDRAWAL' });
+    mutate('/api/conta');
+    reset();
+
+    // i will fix that
+    const modal = document.getElementById('withdrawal-modal') as any;
+    modal.checked = false;
+  };
+
+  return (
+    <>
+      <input type="checkbox" id="withdrawal-modal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <label
+            htmlFor="withdrawal-modal"
+            className="absolute btn btn-sm btn-circle right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">Insira o valor da retirada</h3>
+          <form
+            onSubmit={handleSubmit((data) =>
+              handleSubmitWithdrawal(data as WithdrawalFormData),
+            )}
+          >
+            <div className="py-4">
+              <Input
+                {...register('amount')}
+                name="amount"
+                type="number"
+                label="Valor"
+                step=".01"
+                error={errors.amount}
+              />
+            </div>
+            <div className="mt-0 modal-action">
+              <button
+                type="submit"
+                className={`btn btn-primary ${isSubmitting && 'loading'}`}
+                disabled={!isValid}
+              >
+                Confirmar
+              </button>
+              <label htmlFor="withdrawal-modal cursor-pointer" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,23 @@
+import AccountHistoryList from './AccountHistoryList';
+import Balance from './Balance';
 import ErrorToast from './ErrorToast';
 import Button from './Form/Button';
 import Input from './Form/Input';
 import Header from './Header';
 import Hero from './Hero';
 import LoginForm from './LoginForm';
+import DepositModal from './Modals/DepositModal';
+import WithdrawModal from './Modals/WithdrawalModal';
 
-export { Input, Hero, LoginForm, Button, ErrorToast, Header };
+export {
+  Input,
+  Hero,
+  LoginForm,
+  Button,
+  ErrorToast,
+  Header,
+  Balance,
+  DepositModal,
+  WithdrawModal,
+  AccountHistoryList,
+};

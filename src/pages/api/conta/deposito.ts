cfdbde1602b4c5270134c457256403a4d791daf2
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-18 14:01:36 -0300

    feat(route): add post method on /api/conta/deposito and /api/conta//retirada

---
+++ src/pages/api/conta/deposito.ts
@@ -0,0 +1,19 @@
+import { prisma } from '@/lib/prisma';
+import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
+
+export default async function handler(req: Req, res: Res) {
+  const { email, type, amount } = req.body;
+
+  const user = await prisma.client.findFirst({
+    where: { email },
+  });
+
+  const balance = Number(user?.balance) + amount;
+
+  const response = await prisma.client.update({
+    where: { email },
+    data: { balance, clientHistory: { create: { type, amount } } },
+  });
+
+  res.json(response);
+}

7a7bdb8f132467e2c48139df1055d33b09370eee
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-20 13:40:57 -0300

    feat(route): add /post method on /api/investimentos/comprar

---
+++ src/pages/api/investimentos/comprar.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/lib/prisma';
+import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
+
+export default async function handler(req: Req, res: Res) {
+  const { assetId, purchaseQuantity, clientId } = req.body;
+
+  const client = await prisma.client.findFirst({
+    where: { clientId },
+  });
+
+  const asset = await prisma.availableAssets.findFirst({
+    where: { assetId },
+  });
+
+  const balance =
+    Number(client?.balance) - Number(asset?.unitPrice) * purchaseQuantity;
+  const updatedAssetAmount = Number(asset?.amount) - purchaseQuantity;
+
+  const response = await prisma.client.update({
+    where: { clientId },
+    data: {
+      balance,
+      transactions: { create: { assetId, amount: purchaseQuantity } },
+    },
+  });
+
+  const updatedAsset = await prisma.availableAssets.update({
+    where: { assetId },
+    data: { amount: updatedAssetAmount },
+  });
+
+  res.json({ balance: response.balance, updatedAsset });
+}

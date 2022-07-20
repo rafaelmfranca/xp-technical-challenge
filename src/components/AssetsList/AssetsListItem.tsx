b9044c163308af1bd8abcfce5d0fa3e7d866b6a6
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-20 13:43:18 -0300

    feat: add assets list item component

---
+++ src/components/AssetsList/AssetsListItem.tsx
@@ -0,0 +1,82 @@
+import { formatCurrency } from '@/helpers/formatCurrency';
+import useAccount from '@/hooks/useAccount';
+import { Asset } from '@/types/shared';
+import Image from 'next/image';
+import { ArrowDown, ArrowUp } from 'phosphor-react';
+
+type AssetsListItemProps = {
+  asset: Asset;
+  handleAssetToBuy: (assetId: string) => void;
+};
+
+type CustomLoaderProps = {
+  src: string;
+};
+
+const customLoader = ({ src }: CustomLoaderProps) => {
+  return `https://pro.clear.com.br/src/assets/symbols_icons/${src}`;
+};
+
+export default function AssetsListItem({
+  asset,
+  handleAssetToBuy,
+}: AssetsListItemProps) {
+  const { balance } = useAccount();
+
+  return (
+    <tr key={asset.ticker}>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="w-5 h-5 sm:w-6 sm:h-6 mask mask-squircle">
+              <Image
+                loader={customLoader}
+                src={`${asset.ticker.slice(0, 4)}.png`}
+                width={24}
+                height={24}
+                alt={`Ativo ${asset.ticker}`}
+              />
+            </div>
+          </div>
+          <div className="text-sm font-medium">
+            <div className="font-medium">{asset.ticker}</div>
+            <div>
+              {Number(asset.variation) > 0 ? (
+                <div className="flex items-center gap-1 text-success">
+                  <ArrowUp size={12} />
+                  {Number(asset.variation).toFixed(2)}
+                </div>
+              ) : (
+                <div className="flex items-center gap-1 text-error">
+                  <ArrowDown size={12} />
+                  {Number(asset.variation).toFixed(2)}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </td>
+      <td>
+        <span className="font-medium badge badge-ghost badge-md">
+          {asset.amount}
+        </span>
+      </td>
+      <td>
+        <span className="font-medium badge badge-ghost badge-md">
+          {formatCurrency(Number(asset.unitPrice))}
+        </span>
+      </td>
+      <td className="sm:p-0">
+        <button
+          className="btn btn-success btn-xs"
+          disabled={balance < Number(asset.unitPrice)}
+          onClick={() => handleAssetToBuy(asset.assetId)}
+        >
+          <label htmlFor="asset-purchase-modal" className="cursor-pointer">
+            Compra
+          </label>
+        </button>
+      </td>
+    </tr>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import AccountHistoryList from './AccountHistoryList';
+import AssetOverview from './AssetOverview';
+import AssetsList from './AssetsList';
 import Balance from './Balance';
 import ErrorToast from './ErrorToast';
 import Button from './Form/Button';
@@ -6,8 +8,10 @@ import Input from './Form/Input';
 import Header from './Header';
 import Hero from './Hero';
 import LoginForm from './LoginForm';
+import AssetPurchaseModal from './Modals/AssetPurchaseModal';
 import DepositModal from './Modals/DepositModal';
 import WithdrawModal from './Modals/WithdrawalModal';
+import TabSwitcher from './TabSwitcher';
 
 export {
   Input,
@@ -20,4 +24,8 @@ export {
   DepositModal,
   WithdrawModal,
   AccountHistoryList,
+  AssetsList,
+  TabSwitcher,
+  AssetPurchaseModal,
+  AssetOverview,
 };

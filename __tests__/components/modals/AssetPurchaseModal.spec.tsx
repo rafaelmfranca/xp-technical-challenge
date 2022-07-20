89c8a9d710d5e5ec445a2465768731d21287ceec
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-20 14:04:14 -0300

    test(wip): add basic tests for the asset purchase modal

---
+++ __tests__/components/modals/AssetPurchaseModal.spec.tsx
@@ -0,0 +1,25 @@
+import AccountContext from '@/contexts/account/context';
+import { AssetPurchaseModal } from '@components';
+import { render, screen } from '@testing-library/react';
+import contextValue from '__mocks__/accountContext';
+
+describe('✅ <AssetPurchaseModal />', () => {
+  describe('➡️ Render', () => {
+    it('should render correctly', () => {
+      render(
+        <AccountContext.Provider value={contextValue}>
+          <AssetPurchaseModal desiredAsset={'cl5rc8b3l0172zyrye61wuazw'} />
+        </AccountContext.Provider>,
+      );
+
+      expect(
+        screen.getByRole('heading', { name: /comprar ação/i }),
+      ).toBeInTheDocument();
+      expect(screen.getByText(/quantidade/i)).toBeInTheDocument();
+      expect(screen.getByRole('spinbutton')).toBeInTheDocument();
+      expect(
+        screen.getByRole('button', { name: /confirmar/i }),
+      ).toBeInTheDocument();
+    });
+  });
+});

366c6deb2ecd14ed08453fb1af00fb70354f5c4a
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-20 20:17:36 -0300

    refactor(route): add order by in /api/ativos

--- src/pages/api/ativos/index.ts
+++ src/pages/api/ativos/index.ts
@@ -4,6 +4,7 @@ import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
 export default async function handler(_req: Req, res: Res) {
   const availableAssets = await prisma.availableAssets.findMany({
     where: { amount: { gte: 1 } },
+    orderBy: { ticker: 'asc' },
   });
 
   res.json(availableAssets);

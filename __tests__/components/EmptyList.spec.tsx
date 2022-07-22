ad8bf3b183211fd97715f925717e36ab0f1c6f45
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-22 17:24:23 -0300

    test: add tests for the empty list component

---
+++ __tests__/components/EmptyList.spec.tsx
@@ -0,0 +1,36 @@
+import { EmptyList } from '@components';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('✅ <EmptyList />', () => {
+  const handleTabChange = jest.fn().mockResolvedValueOnce(1);
+
+  describe('➡️ Render', () => {
+    it('should render correctly', () => {
+      render(<EmptyList message="Some message" />);
+
+      expect(screen.getByRole('img', { name: /atenção/i })).toBeInTheDocument();
+      expect(screen.getByText(/some message/i)).toBeInTheDocument();
+    });
+
+    it('should render a button when "handleTabChange" prop is passed', () => {
+      render(<EmptyList message="Some message" handleTabChange={handleTabChange} />);
+
+      expect(
+        screen.getByRole('button', { name: /veja as opções de ativos!/i }),
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe('➡️ Behavior', () => {
+    it('should call "handleTabChange" when button is clicked', async () => {
+      render(<EmptyList message="Some message" handleTabChange={handleTabChange} />);
+
+      const button = screen.getByRole('button', { name: /veja as opções de ativos!/i });
+      await userEvent.click(button);
+
+      expect(handleTabChange).toHaveBeenCalledTimes(1);
+      expect(handleTabChange).toHaveBeenCalledWith(1);
+    });
+  });
+});

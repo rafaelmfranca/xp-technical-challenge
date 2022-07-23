e2165fdf4c438be03d7a0bc1ed3dcfc9363fd0fd
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-07-23 20:00:26 -0300

    fix: add max value in deposit schema

--- src/schemas/deposit.ts
+++ src/schemas/deposit.ts
@@ -1,7 +1,12 @@
 import * as yup from 'yup';
 
 const depositSchema = yup.object().shape({
-  amount: yup.number().typeError('').positive('O valor deve ser positivo').required(),
+  amount: yup
+    .number()
+    .typeError('')
+    .positive('O valor deve ser positivo')
+    .max(1000000, 'Valor máximo por transação é R$1.000.000')
+    .required(),
 });
 
 export default depositSchema;
